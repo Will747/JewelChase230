6208a5be72a66476ca57897301e2418ebac6569d
cos-glitch <77969841+cos-glitch@users.noreply.github.com>
2022-11-22 16:38:33 +0000

    Ai character and Player backbone added

---
+++ src/main/java/com/example/jewelchase230/AICharacter.java
@@ -0,0 +1,13 @@
+package com.example.jewelchase230;
+
+public class AICharacter extends Character{
+    /**
+     * Constructs a renderable component.
+     *
+     * @param x X position on the grid.
+     * @param y Y position on the grid.
+     */
+    public AICharacter(int x, int y) {
+        super(x, y);
+    }
+}
