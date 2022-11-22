3c69f080b66d0b07da02433e9f34ee03a939fcfa
cos-glitch <77969841+cos-glitch@users.noreply.github.com>
2022-11-22 16:25:47 +0000

    Character added

---
+++ src/main/java/com/example/jewelchase230/Character.java
@@ -0,0 +1,18 @@
+package com.example.jewelchase230;
+
+/**
+ * Renders the characters to the game.
+ *
+ * @author carolinesegestahl
+ */
+public class Character extends Sprite {
+    /**
+     * Constructs a renderable component.
+     *
+     * @param x X position on the grid.
+     * @param y Y position on the grid.
+     */
+    public Character(int x, int y) {
+        super(x, y);
+    }
+}
---
+++ src/main/java/com/example/jewelchase230/Sprite.java
@@ -0,0 +1,40 @@
+package com.example.jewelchase230;
+
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * Represents Sprites in the game.
+ *
+ * @author carolinesegestahl
+ */
+public class Sprite extends Renderable{
+    /**
+     * Constructs a renderable component.
+     *
+     * @param x X position on the grid.
+     * @param y Y position on the grid.
+     */
+    public Sprite(int x, int y) {
+        super(x, y);
+    }
+
+    /**
+     * Called just before the grid gets re-rendered.
+     *
+     * @param time Time since last frame in milliseconds.
+     */
+    @Override
+    public void tick(int time) {
+
+    }
+
+    /**
+     * Draws this item to the canvas.
+     *
+     * @param gc GraphicsContext for creating draw class.
+     */
+    @Override
+    public void draw(GraphicsContext gc) {
+
+    }
+}
