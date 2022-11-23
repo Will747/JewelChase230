bfc391d398c09cc22263d20f8e2b2ebe0491b36c
Adam <adam_smith_03@outlook.com>
2022-11-23 13:59:41 +0000

    Added method to solve the puzzle

--- src/main/java/com/example/jewelchase230/MessageOfTheDay.java
+++ src/main/java/com/example/jewelchase230/MessageOfTheDay.java
@@ -14,12 +14,12 @@ public class MessageOfTheDay
 
     private static String getPuzzle()
     {
+        String puzzle = "";
         try
         {
             URL puzzleURL = new URL("http://cswebcat.swansea.ac.uk/puzzle");
             HttpURLConnection puzzleConnection = (HttpURLConnection)puzzleURL.openConnection();
             puzzleConnection.setRequestMethod("GET");
-            String puzzle = "";
 
             InputStreamReader puzzleInputReader = new InputStreamReader(puzzleConnection.getInputStream());
             BufferedReader puzzleBufferedReader = new BufferedReader(puzzleInputReader);
@@ -32,16 +32,56 @@ public class MessageOfTheDay
             
             puzzleInputReader.close();
             puzzleBufferedReader.close();
+            puzzle = puzzleResponse.toString();
         }
         catch (Exception e)
         {
             System.out.println("Error connecting to recieve puzzle.");
         }
-
+        solvePuzzle(puzzle);
     }
 
-    private static String getSolution()
+    private static String solvePuzzle(String puzzle)
     {
+        final int SUFFIX_LENGTH = 6;
+        final String SUFFIX = "CS-230";
+        String solvedPuzzle = "";
 
+        for (int i = 0; i < puzzle.length(); i++)
+        {
+            int temp = i;
+            if ((temp + 1) % 2 == 0)
+            {
+                if ((((int) puzzle.charAt(i)) + (temp + 1)) > 90)
+                {
+                   solvedPuzzle =solvedPuzzle + ((char) (((65 + (temp + 1 - (( 90 - ((int) puzzle.charAt(i)))))))));
+                }
+                else
+                {
+                   solvedPuzzle =solvedPuzzle + ((char) (((int) puzzle.charAt(i)) + (temp + 1)));
+                }
+            }
+            else
+            {
+                if ((((int) puzzle.charAt(i)) - (temp + 1)) < 65)
+                {
+                   solvedPuzzle =solvedPuzzle + ((char) (90 - ((temp + 1) - (((int) puzzle.charAt(i)) - 65))));
+                }
+                else
+                {
+                   solvedPuzzle =solvedPuzzle + ((char) (((int) puzzle.charAt(i)) - (temp + 1)));
+                }
+            }
+        }
+
+        int solvedPuzzleLength = puzzle.length() + SUFFIX_LENGTH;
+        String solvedPuzzleLengthString = "" + solvedPuzzleLength;
+        String solutionQuery = solvedPuzzleLengthString + solvedPuzzle + SUFFIX;
+        getSolution(solutionQuery);
+    }
+
+    private static String getSolution(String query)
+    {
+        
     }
 }
