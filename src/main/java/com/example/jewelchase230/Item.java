1b5a8264d9f350b0464c140a541c56322ad555ee
Scott371 <2122851@swansea.ac.uk>
2022-11-23 14:20:04 +0000

    Updated Items, loot, door, gate

--- src/main/java/com/example/jewelchase230/Item.java
+++ src/main/java/com/example/jewelchase230/Item.java
@@ -4,17 +4,23 @@ import com.example.jewelchase230.vectors.IntVector2D;
 
 public class Item extends Sprite {
 
-    public Item(int x, int y) {
+    public Item(int x, int y, String name, String fileName) {
         super(x, y);
+        this.name = name;
+        this.fileName = fileName;
     }
 
-    public Item(String name, IntVector2D gridPos) {
+    /*public Item(String name, IntVector2D gridPos) {
         super(gridPos.getX(), gridPos.getY());
 
-    }
+    }*/
 
-    private String name;
-    private String fileName;
+    public String name;
+    public String fileName;
+
+    public void remove(Item item){
+        item.setGridPosition(new IntVector2D(-1,-1));
+    }
 
     public void setName(String name) {
         this.name = name;
