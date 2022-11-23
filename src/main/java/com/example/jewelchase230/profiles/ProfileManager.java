25067024e4491702d3745c78f03fd5b6547ea383
kiwi-kr <107872612+kiwi-kr@users.noreply.github.com>
2022-11-23 20:13:51 +0000

    Added methods & comments
    I have updated the Profile.txt to have more meaningful data for testing
    
    Added comments
    
    Added saveProfile
    Added some error handling
    Note:: None of the while statements iterate upon testing -- not sure why?

--- src/main/java/com/example/jewelchase230/profiles/ProfileManager.java
+++ src/main/java/com/example/jewelchase230/profiles/ProfileManager.java
@@ -3,6 +3,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+/**
+ * This class manages the handling of Profiles, including the .txt files of which
+ * Profiles will be written/read from during saving and loading.
+ *
+ * @author Kellie Robinson
+ */
 
 public class ProfileManager
 {
@@ -10,10 +20,14 @@ public class ProfileManager
 	private String playerName;
 	private Integer levelReached;
 	private Integer overallScore;
+	private Integer uniquePlayerID;
+	private static int lineCount;
+	static int uniqueIDFromFile;
 	
 
 	// we may need another variable here Integer uniquePlayerID ? depends on how we want to uniquely identify users
-	  public ProfileManager(String playerProfileSlot, String playerName, Integer levelReached, Integer overallScore) {
+	  public ProfileManager(Integer uniquePlayerID, String playerProfileSlot, String playerName, Integer levelReached, Integer overallScore) {
+		  this.uniquePlayerID = uniquePlayerID;
 		  this.playerProfileSlot = playerProfileSlot;
 		  this.playerName = playerName; 
 		  this.levelReached = levelReached;
@@ -21,9 +35,15 @@ public class ProfileManager
 		  
 	}
 
-	public static void main(final String[] args)
+	public static void main(final String[] args) throws IOException
 	    {
-	        readLines();
+		//tests to see if functions work 
+		ProfileManager Nicole = new ProfileManager (1, "P1" , "Nicole", 4, 342);
+		ProfileManager Kiwi = new ProfileManager (4, "P4", "FART", 5, 500); 
+		
+		readLines(); 
+		saveProfile(Kiwi); 
+		System.out.println("Hi"); 
 	    }
 	  
 	  
@@ -53,17 +73,26 @@ public class ProfileManager
     */
 
     // Reads each line of the text file "Profile.txt"
-    public static void readLines()
+	 /**
+     * Reads content from file and stores them appropriately to be handled
+     * in the makeProfile method
+     * @return lineDataSplit
+     * @throws FileNotFoundException
+     */
+    public static String[] readLines()
     {
+    	 String[] lineDataSplit = null;
         try
         {
             File myFile = new File("Profiles.txt");
             Scanner input = new Scanner(myFile);
-            while (input.hasNextLine())
-            {
+            System.out.println("readfile");
+            while (input.hasNextLine()) {
+            	lineCount++;
+            	System.out.println(lineCount + " " + "hiii");
                 String data = input.nextLine();
-                String[] lineDataSplit = data.split("."); 
-                
+                lineDataSplit = data.split("."); 
+                 uniqueIDFromFile = Integer.parseInt(lineDataSplit[0]);
                 //temporary print statement
                 System.out.println(data);
             }
@@ -75,10 +104,17 @@ public class ProfileManager
             System.out.println("An error occurred.");
             e.printStackTrace();
         } 
+        return lineDataSplit;
     }
+    /**
+     * Constructs a profile object
+     * 
+     * @param String[] lineDataSplit
+     * @return profile
+     */
     
-    //makes a profile from the lineDataSplit array
     public static ProfileManager makeProfile(String[] lineDataSplit) {
+    	
     	ArrayList<Integer>profileManagerIntegers = new ArrayList<Integer>(); 
     	for (int i = 0; i < lineDataSplit.length; i++) { 
     		if (i == 2 || i == 3) { 
@@ -87,15 +123,44 @@ public class ProfileManager
     		}
     		
     	}
-    	ProfileManager profile = new ProfileManager(lineDataSplit[0],lineDataSplit[1],profileManagerIntegers.get(0), profileManagerIntegers.get(1));
+    	ProfileManager profile = new ProfileManager((lineCount + 1), lineDataSplit[0],lineDataSplit[1],profileManagerIntegers.get(0), profileManagerIntegers.get(1));
     	return profile;
     }
     
-    public void saveProfile(ProfileManager profile) { 
+    
+    /**
+     * Overwrites the Profiles.txt file with updated Profile information
+     * 
+     * @param ProfileManager profile
+     * @throws FileNotFoundException
+     */
+    public static void saveProfile(ProfileManager profile) throws IOException { 
     	 try
          {
-             File myFile = new File("Profiles.txt");
-             Scanner input = new Scanner(myFile);
+    		 FileWriter writeToPlayerFile = new FileWriter ("Profiles.txt");
+    		 Scanner input = new Scanner(new File("Profiles.txt"));
+    		 StringBuffer buffer = new StringBuffer();
+    		 System.out.println("Before While");
+    		 while (input.hasNextLine()) {
+    			 System.out.println("While");
+    			 buffer.append(input.nextLine()+System.lineSeparator());
+    			 
+    			 if (profile.getUniquePlayerID() == uniqueIDFromFile) { 
+        			 System.out.println("If");
+
+    				 String overwrittenData = input.nextLine();
+    				 String overwritingData = (profile.getUniquePlayerID() + "." + profile.getPlayerProfileSlot() + "." + profile.getPlayerName() + "." + profile.getLevelReached() + "." +
+    						 profile.getOverallScore()); 
+    				 String fileContents = buffer.toString();
+    				 fileContents = fileContents.replaceAll(overwrittenData, overwritingData);
+    				 
+    				 writeToPlayerFile.append(fileContents); 
+    				 writeToPlayerFile.flush(); 
+    				 
+    			 }  
+             }
+    		 
+             
              input.close();
          }
      
@@ -107,25 +172,79 @@ public class ProfileManager
     	 
     }
     
-    public String getplayerProfileSlot(ProfileManager profile) {
-    	return this.playerProfileSlot; 
-    }
+
+
+	/**
+	 * @return the playerProfileSlot
+	 */
+	public String getPlayerProfileSlot() {
+		return playerProfileSlot;
+	}
+
+	/**
+	 * @return the playerName
+	 */
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	/**
+	 * @return the levelReached
+	 */
+	public Integer getLevelReached() {
+		return levelReached;
+	}
+
+	/**
+	 * @return the overallScore
+	 */
+	public Integer getOverallScore() {
+		return overallScore;
+	}
+
+	/**
+	 * @param playerProfileSlot the playerProfileSlot to set
+	 */
+	public void setPlayerProfileSlot(String playerProfileSlot) {
+		this.playerProfileSlot = playerProfileSlot;
+	}
+
+	/**
+	 * @param playerName the playerName to set
+	 */
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	/**
+	 * @param levelReached the levelReached to set
+	 */
+	public void setLevelReached(Integer levelReached) {
+		this.levelReached = levelReached;
+	}
+
+	/**
+	 * @param overallScore the overallScore to set
+	 */
+	public void setOverallScore(Integer overallScore) {
+		this.overallScore = overallScore;
+	}
+
+	/**
+	 * @return the uniquePlayerID
+	 */
+	public Integer getUniquePlayerID() {
+		return uniquePlayerID;
+	}
+
+	/**
+	 * @param uniquePlayerID the uniquePlayerID to set
+	 */
+	public void setUniquePlayerID(Integer uniquePlayerID) {
+		this.uniquePlayerID = uniquePlayerID;
+	}
     
     
-    public String getPlayerName(ProfileManager profile) {
-    	return this.playerName;
-    }
     
-    public Integer getLevelReacher(ProfileManager profile) {
-    	return this.levelReached;
-    }
-    
-    public Integer getOverallScore(ProfileManager profile) { 
-    	return this.overallScore;
-    }
-    
-    
-    
-/// Kiwi: Add setters
 
 }
