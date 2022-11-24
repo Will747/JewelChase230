7428584c429593be49f3ab0181c469304b1a899a
cos-glitch <77969841+cos-glitch@users.noreply.github.com>
2022-11-24 10:02:02 +0000

    fixed door issues

--- src/main/java/com/example/jewelchase230/Door.java
+++ src/main/java/com/example/jewelchase230/Door.java
@@ -1,9 +1,12 @@
 package com.example.jewelchase230;
 
 public class Door extends Item {
-
-    public Door(int x, int y) {
-        super(x, y);
+    public Door(int x, int y, String name, String fileName) {
+        super(x, y, name, fileName);
     }
-    
+
+//    public Door(int x, int y) {
+//        super(x, y);
+//    }
+//
 }
