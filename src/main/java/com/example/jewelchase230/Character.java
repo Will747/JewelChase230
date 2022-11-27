53a738ffba429c86d30c6560cbb53f14b1d805db
cos-glitch <77969841+cos-glitch@users.noreply.github.com>
2022-11-27 16:44:49 +0000

    implemented check for colour

--- src/main/java/com/example/jewelchase230/Character.java
+++ src/main/java/com/example/jewelchase230/Character.java
@@ -1,5 +1,6 @@
 package com.example.jewelchase230;
 
+import com.example.jewelchase230.vectors.IntVector2D;
 import javafx.scene.canvas.GraphicsContext;
 
 /**
@@ -26,9 +27,14 @@ public abstract class Character extends Sprite {
      * @return a boolean, true or false.
      */
     protected boolean canMoveUp(int x, int y) {
-
-        return getLevel().getLevelSize().getY() < getGridPosition().getY();
-        //if tile color is same and no interrupting items then make the move
+        IntVector2D yTry = new IntVector2D(getGridPosition().getX(), getGridPosition().getY() + 1);
+        if (getLevel().getLevelSize().getY() < getGridPosition().getY()) {
+            if (getLevel().getTile(getGridPosition()).getTop() == getLevel().getTile(yTry).getBottom()) {
+                return true;
+            }
+        }
+        // no interrupting items then make the move
+        return false;
     }
 
     /**
@@ -39,9 +45,14 @@ public abstract class Character extends Sprite {
      * @return a boolean, true or false.
      */
     protected boolean canMoveDown(int x, int y) {
-
-        return getLevel().getLevelSize().getY() > getGridPosition().getY();
-        //if tile color is same and no interrupting items then make the move
+        IntVector2D yTry = new IntVector2D(getGridPosition().getX(), getGridPosition().getY() - 1);
+        if (getLevel().getLevelSize().getY() > getGridPosition().getY()) {
+            if (getLevel().getTile(getGridPosition()).getBottom() == getLevel().getTile(yTry).getTop()){
+                return true;
+            }
+        }
+        return false;
+        //no interrupting items then make the move
     }
 
     /**
@@ -52,9 +63,14 @@ public abstract class Character extends Sprite {
      * @return a boolean, true or false.
      */
     protected boolean canMoveRight(int x, int y) {
-
-        return getLevel().getLevelSize().getX() > getGridPosition().getX();
-        //if tile color is same and no interrupting items then make the move
+        IntVector2D xTry = new IntVector2D(getGridPosition().getX() + 1, getGridPosition().getY());
+        if (getLevel().getLevelSize().getX() > getGridPosition().getX()) {
+            if (getLevel().getTile(getGridPosition()).getRight() == getLevel().getTile(xTry).getRight()) {
+                return true;
+            }
+        }
+        return false;
+        //if no interrupting items then make the move
     }
 
     /**
@@ -65,8 +81,14 @@ public abstract class Character extends Sprite {
      * @return a boolean, true or false
      */
     protected boolean canMoveLeft(int x, int y) {
-        return getLevel().getLevelSize().getX() < getGridPosition().getX();
-        //if tile color is same and no interrupting items then make the move
+        IntVector2D xTry = new IntVector2D(getGridPosition().getX() - 1, getGridPosition().getY());
+        if (getLevel().getLevelSize().getX() < getGridPosition().getX()) {
+            if (getLevel().getTile(getGridPosition()).getLeft() == getLevel().getTile(xTry).getLeft()) {
+                return true;
+            }
+        }
+        return false;
+        //if no interrupting items then make the move
     }
 
     /**
