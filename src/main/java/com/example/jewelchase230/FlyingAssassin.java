8335e5026178a9978af80e882a457cbebea21731
cos-glitch <77969841+cos-glitch@users.noreply.github.com>
2022-11-28 13:58:02 +0000

    started FA

--- src/main/java/com/example/jewelchase230/FlyingAssassin.java
+++ src/main/java/com/example/jewelchase230/FlyingAssassin.java
@@ -17,4 +17,25 @@ public class FlyingAssassin extends AICharacter {
     protected void getNextMove(IntVector2D move) {
         super.getNextMove(move);
     }
+
+    private IntVector2D move() {
+        if (getGridPosition().getX() < getLevel().getLevelSize().getX()) {
+
+        }
+        while (getGridPosition() < getLevel().getLevelSize())
+
+        return new IntVector2D(x,y);
+    }
 }
+
+//        It moves in a straight line in
+//        the direction it is facing until it reaches the edge
+//        of the level. On reaching the edge, it rotates 180
+//        degrees and continues moving forward. As this
+//        NPC is flying, it does not respect the floor move-
+//        ment rules and simply always travels straight. The
+//        movement is either vertical or horizontal.
+//        If the Flying Assassin connects with (i.e. occupies
+//        the same tile) the player, the player loses. If they
+//        connect with another NPC, that NPC is removed
+//        from the game.
