f9d46400c9f11c39db61fd34c9d9972f5b9b2b0a
Will747 <me@wkaye.dev>
2022-11-29 14:56:26 +0000

    Fix LevelTest

--- src/test/java/JewelChaseTests/LevelTest.java
+++ src/test/java/JewelChaseTests/LevelTest.java
@@ -20,7 +20,7 @@ public class LevelTest {
         Level testLevel = new Level(new IntVector2D(5, 5));
 
         IntVector2D itemPos = new IntVector2D(0, 0);
-        testLevel.addItem(itemPos, new Bomb(0, 0, "Bomb", "?", 5));
+        testLevel.addItem(itemPos, new Bomb(5));
 
         assertNotNull(testLevel.getItem(itemPos));
     }
@@ -29,9 +29,9 @@ public class LevelTest {
     public void getItemsOfTypeTest() {
         Level testLevel = new Level(new IntVector2D(5, 5));
 
-        testLevel.addItem(new IntVector2D(0, 0), new Bomb(0, 0, "Bomb", "?", 5));
-        testLevel.addItem(new IntVector2D(0, 1), new Bomb(0, 1, "Bomb2", "?", 5));
-        testLevel.addItem(new IntVector2D(0, 2), new Gate(0, 2, "Gate", "?", "Red"));
+        testLevel.addItem(new IntVector2D(0, 0), new Bomb(5));
+        testLevel.addItem(new IntVector2D(0, 1), new Bomb(5));
+        testLevel.addItem(new IntVector2D(0, 2), new Gate("Red"));
 
         ArrayList<Bomb> bombs = testLevel.getAllItemsOfType(Bomb.class);
         assertEquals(2, bombs.size());
@@ -41,9 +41,9 @@ public class LevelTest {
     public void getAllItemsTest() {
         Level testLevel = new Level(new IntVector2D(5, 5));
 
-        testLevel.addItem(new IntVector2D(0, 0), new Bomb(0, 0, "Bomb", "?", 5));
-        testLevel.addItem(new IntVector2D(0, 1), new Bomb(0, 1, "Bomb2", "?", 5));
-        testLevel.addItem(new IntVector2D(0, 2), new Gate(0, 2, "Gate", "?", "Red"));
+        testLevel.addItem(new IntVector2D(0, 0), new Bomb(5));
+        testLevel.addItem(new IntVector2D(0, 1), new Bomb(5));
+        testLevel.addItem(new IntVector2D(0, 2), new Gate("Red"));
 
         ArrayList<Item> items = testLevel.getAllItems();
         assertEquals(3, items.size());
