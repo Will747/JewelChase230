b8a8859ddc9aa02ffcf0bbbbf737ae6965d44f04
Will747 <me@wkaye.dev>
2022-12-01 12:11:05 +0000

    Improve checkstyle warnings on Tile

--- src/main/java/com/example/jewelchase230/Tile.java
+++ src/main/java/com/example/jewelchase230/Tile.java
@@ -7,7 +7,7 @@ import com.example.jewelchase230.items.Item;
 import com.example.jewelchase230.vectors.DoubleVector2D;
 
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.*;
+import javafx.scene.image.Image;
 
 /**
  * A tile is one square on the grid that can have a different colour for
@@ -31,20 +31,20 @@ public final class Tile extends Renderable {
 
     /**
      * Constructs a new tile.
-     * @param inTop Top colour
-     * @param inBottom Bottom colour
-     * @param inLeft Left colour
-     * @param inRight Right colour
+     * @param inTopLeft Top left colour
+     * @param inTopRight Top right colour
+     * @param inBottomLeft Bottom left colour
+     * @param inBottomRight Bottom right colour
      */
-    public Tile(final TileColour topLeft,
-                final TileColour topRight,
-                final TileColour bottomLeft,
-                final TileColour bottomRight) {
+    public Tile(final TileColour inTopLeft,
+                final TileColour inTopRight,
+                final TileColour inBottomLeft,
+                final TileColour inBottomRight) {
         super();
-        this.topLeft = topLeft;
-        this.topRight = topRight;
-        this.bottomLeft = bottomLeft;
-        this.bottomRight = bottomRight;
+        topLeft = inTopLeft;
+        topRight = inTopRight;
+        bottomLeft = inBottomLeft;
+        bottomRight = inBottomRight;
     }
 
     /**
@@ -62,30 +62,42 @@ public final class Tile extends Renderable {
         item = inItem;
     }
 
-    public TileColour getTopLeft(){
+    /**
+     * @return Top left part of the tile.
+     */
+    public TileColour getTopLeft() {
         return topLeft;
     }
 
-    public TileColour getTopRight(){
+    /**
+     * @return Top right part of tile.
+     */
+    public TileColour getTopRight() {
         return topRight;
     }
 
-    public TileColour getBottomLeft(){
+    /**
+     * @return Bottom left part of tile.
+     */
+    public TileColour getBottomLeft() {
         return bottomLeft;
     }
 
-    public TileColour getBottomRight(){
+    /**
+     * @return Bottom right part of tile.
+     */
+    public TileColour getBottomRight() {
         return bottomRight;
     }
 
     /**
      * Draws the sqaures based on the parameters passed.
      * @param gc   The graphic context.
-     * @param img   The image being applied to the tile. 
+     * @param img   The image being applied to the tile.
      * @param pos   The position in which the tile is being rendered.
      * @param sideLength The length of the tile.
      */
-    private void drawSquare(GraphicsContext gc, Image img,  DoubleVector2D pos, double sideLength){
+    private void drawSquare(GraphicsContext gc, Image img, DoubleVector2D pos, double sideLength) {
         double upperX = pos.getX();
         double upperY = pos.getY();
         gc.drawImage(img, upperX, upperY, sideLength, sideLength);
@@ -95,155 +107,153 @@ public final class Tile extends Renderable {
      * Draws the tile has a whole using the drawSquare method to do each quadrant.
      */
     @Override
-    public void draw(final GraphicsContext gc){
+    public void draw(final GraphicsContext gc) {
         DoubleVector2D pos = getRenderPosition();
         double size = getCubeSize();
         Image img;
 
         double sideLength = size / 2;
         
-        /**
-         * Makes and passes the variables needed for the tile to be drawn for each quadrant.
-         */
+        /* Makes and passes the variables needed for the tile to be drawn for each quadrant. */
         DoubleVector2D topLeft = new DoubleVector2D(0, size);
         switch(getTopLeft()){
             case Red:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_RED.png"));
                     drawSquare(gc, img, topLeft, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_RED.png file not found");
                 }
                 break;
             case Blue:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_BLUE.png"));
                     drawSquare(gc, img, topLeft, sideLength);
-                }catch(FileNotFoundException e){
+                } catch(FileNotFoundException e) {
                     System.out.println("WOODFLOOR_BLUE.png file not found");
                 }
                 break;
             case Green:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_GREEN.png"));
                     drawSquare(gc, img, topLeft, sideLength);
-                }catch (FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_GREEN.png file not found");
                 }
                 break;
             case Yellow:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_PINK.png"));
                     drawSquare(gc, img, topLeft, sideLength);
-                }catch(FileNotFoundException e){
+                } catch(FileNotFoundException e){
                     System.out.println("WOODFLOOR_PINK.png file not found");
                 }
                 break;
         }
 
         DoubleVector2D topRight = new DoubleVector2D(sideLength, size);
-        switch(getTopRight()){
+        switch (getTopRight()) {
             case Red:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_RED.png"));
                     drawSquare(gc, img, topRight, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_RED.png file not found");
                 }
                 break;
             case Blue:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_BLUE.png"));
                     drawSquare(gc, img, topRight, sideLength);
-                }catch(FileNotFoundException e){
+                } catch(FileNotFoundException e) {
                     System.out.println("WOODFLOOR_BLUE.png file not found");
                 }
                 break;
             case Green:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_GREEN.png"));
                     drawSquare(gc, img, topRight, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_GREEN.png file not found");
                 }
                 break;
             case Yellow:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_PINK.png"));
                     drawSquare(gc, img, topRight, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_PINK.png file not found");
                 }
                 break;
         }
 
         DoubleVector2D bottomLeft = new DoubleVector2D(0, sideLength);
-        switch(getBottomLeft()){
+        switch (getBottomLeft()) {
             case Red:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_RED.png"));
                     drawSquare(gc, img, bottomLeft, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_RED.png file not found");
                 }
                 break;
             case Blue:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_BLUE.png"));
                     drawSquare(gc, img, bottomLeft, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_BLUE.png file not found");
                 }
                 break;
             case Green:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_GREEN.png"));
                     drawSquare(gc, img, bottomLeft, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_GREEN.png file not found");
                 }
                 break;
             case Yellow:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_PINK.png"));
                     drawSquare(gc, img, bottomLeft, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_PINK.png file not found");
                 }
                 break;
         }
 
         DoubleVector2D bottomRight = new DoubleVector2D(sideLength, sideLength);
-        switch(getBottomRight()){
+        switch (getBottomRight()) {
             case Red:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_RED.png"));
                     drawSquare(gc, img, bottomRight, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_RED.png file not found");
                 }
                 break;
             case Blue:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_BLUE.png"));
                     drawSquare(gc, img, bottomRight, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_BLUE.png file not found");
                 }
                 break;
             case Green:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_GREEN.png"));
                     drawSquare(gc, img, bottomRight, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_GREEN.png file not found");
                 }
                 break;
             case Yellow:
-                try{
+                try {
                     img = new Image(new FileInputStream("images/WOODFLOOR_PINK.png"));
                     drawSquare(gc, img, bottomRight, sideLength);
-                }catch(FileNotFoundException e){
+                } catch (FileNotFoundException e) {
                     System.out.println("WOODFLOOR_PINK.png file not found");
                 }
                 break;
