ac770be0f50e617f62dda2a27c7280634eee6865
Dowen212 <2124746@swansea.ac.uk>
2022-12-03 15:23:46 +0000

    IT CHANGED. LETS GOOOOOOOOOOOOO THIS TOOK ME WAY TO LONG AND I AM NOW ON THE VERGE OF LOSING YEARS OFF MY LIFE FROM THIS ERROR FIX
    we can now change the text of the label on the player buttons. with new controllers.

--- src/main/java/com/example/jewelchase230/menus/ProfileMenu.java
+++ src/main/java/com/example/jewelchase230/menus/ProfileMenu.java
@@ -5,11 +5,7 @@ import java.util.Optional;
 import com.example.jewelchase230.Main;
 import com.example.jewelchase230.Menu;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 /**
  * Controller for the profile menus - changing scenes.
@@ -17,14 +13,10 @@ import javafx.stage.Stage;
  * @author Daniel Clark
  */
 public final class ProfileMenu {
+
     @FXML
     void onBackToMainMenuPressed(final MouseEvent event) {
         Main.switchRoot(Menu.getMainMenu());
-    }
-
-    @FXML
-    void onBackToProfileMenuPressed(final MouseEvent event) {
-        Main.switchRoot(Menu.getProfileMenu());
     }
 
     @FXML
@@ -45,32 +37,5 @@ public final class ProfileMenu {
     @FXML
     void onP4Pressed(final MouseEvent event) {
         Main.switchRoot(Menu.getProfileSelect());
-    }
-
-    @FXML
-    private void onStartGamePressed(final MouseEvent event) {
-        Main.switchToCanvas();
-    }
-
-    @FXML
-    private void onDeletePressed(final MouseEvent event) {
-        Alert.AlertType type = Alert.AlertType.CONFIRMATION;
-
-        Alert alert = new Alert(type, "");
-
-        alert.initModality(Modality.APPLICATION_MODAL);
-        //alert.initOwner(stage);
-        alert.getDialogPane().setContentText("are you sure?");
-        alert.getDialogPane().setHeaderText("You are about to delete this profile");
-
-        Optional<ButtonType> result = alert.showAndWait();
-        if(result.get() == ButtonType.OK)
-        {
-            System.out.println("profile deleted.");
-        }
-        else if (result.get() == ButtonType.CANCEL)
-        {
-            System.out.println("cancelled profile deletion.");
-        }
     }
 }
---
+++ src/main/java/com/example/jewelchase230/menus/ProfileSelectMenu.java
@@ -0,0 +1,66 @@
+package com.example.jewelchase230.menus;
+
+import java.util.Optional;
+
+import com.example.jewelchase230.Main;
+import com.example.jewelchase230.Menu;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+
+/**
+ * Controller for the profile menus - changing scenes.
+ *
+ * @author Daniel Clark
+ */
+public final class ProfileSelectMenu {
+
+    @FXML 
+    private Label playerOne;
+
+    @FXML
+    private void initialize()
+    {
+        playerOne.setText("IT HAS CHANGED!!!!!!!!!!! LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+    }
+
+    @FXML
+    void onBackToMainMenuPressed(final MouseEvent event) {
+        Main.switchRoot(Menu.getMainMenu());
+    }
+
+    @FXML
+    void onBackToProfileMenuPressed(final MouseEvent event) {
+        Main.switchRoot(Menu.getProfileMenu());
+    }
+
+    @FXML
+    private void onStartGamePressed(final MouseEvent event) {
+        Main.switchToCanvas();
+    }
+
+    @FXML
+    private void onDeletePressed(final MouseEvent event) {
+        Alert.AlertType type = Alert.AlertType.CONFIRMATION;
+
+        Alert alert = new Alert(type, "");
+
+        alert.initModality(Modality.APPLICATION_MODAL);
+        //alert.initOwner(stage);
+        alert.getDialogPane().setContentText("are you sure?");
+        alert.getDialogPane().setHeaderText("You are about to delete this profile");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get() == ButtonType.OK)
+        {
+            System.out.println("profile deleted.");
+        }
+        else if (result.get() == ButtonType.CANCEL)
+        {
+            System.out.println("cancelled profile deletion.");
+        }
+    }
+}
