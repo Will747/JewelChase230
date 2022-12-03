44e6d0e5c6085ebea6872ed27b0da537f78e4e03
Dowen212 <2124746@swansea.ac.uk>
2022-12-03 12:29:58 +0000

    Deletion popup

--- src/main/java/com/example/jewelchase230/menus/ProfileMenu.java
+++ src/main/java/com/example/jewelchase230/menus/ProfileMenu.java
@@ -1,9 +1,15 @@
 package com.example.jewelchase230.menus;
 
+import java.util.Optional;
+
 import com.example.jewelchase230.Main;
 import com.example.jewelchase230.Menu;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * Controller for the profile menus - changing scenes
@@ -44,5 +50,27 @@ public final class ProfileMenu {
     @FXML
     private void onStartGamePressed(final MouseEvent event) {
         Main.switchToCanvas();
+    }
+
+    @FXML
+    private void onDeletePressed(final MouseEvent event) {
+        Alert.AlertType type = Alert.AlertType.CONFIRMATION;
+
+        Alert alert = new Alert(type, "");
+
+        alert.initModality(Modality.APPLICATION_MODAL);
+        //alert.initOwner(stage);
+        alert.getDialogPane().setContentText("are you sure?");
+        alert.getDialogPane().setHeaderText("You are about to delete this profile");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get() == ButtonType.OK)
+        {
+            System.out.println("profile deleted.");
+        }
+        else if (result.get() == ButtonType.CANCEL)
+        {
+            System.out.println("cancelled profile deletion.");
+        }
     }
 }
