e009320d017d9f7839205783068d72ce740ecc15
Dowen212 <2124746@swansea.ac.uk>
2022-12-04 15:17:56 +0000

    Selected Profile displays the name of the positioned profile selected

--- src/main/java/com/example/jewelchase230/menus/ProfileMenu.java
+++ src/main/java/com/example/jewelchase230/menus/ProfileMenu.java
@@ -3,7 +3,6 @@ package com.example.jewelchase230.menus;
 import com.example.jewelchase230.Main;
 import com.example.jewelchase230.Menu;
 import com.example.jewelchase230.profiles.ProfileManager;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
@@ -36,23 +35,16 @@ public final class ProfileMenu {
 
 	@FXML
 	private Label playerFour;
-
 	
 	@FXML
 	  private void initialize() {
 
 		playerOne.setText(ProfileManager.listOfProfile.get(0).getPlayerName());
-
 		playerTwo.setText(ProfileManager.listOfProfile.get(1).getPlayerName());
-
 		playerThree.setText(ProfileManager.listOfProfile.get(2).getPlayerName());
-
 		playerFour.setText(ProfileManager.listOfProfile.get(3).getPlayerName());
-		
-
 	}
 	
-
 	@FXML
 	void onBackToMainMenuPressed(final MouseEvent event) {
 		Main.switchRoot(Menu.getMainMenu());
@@ -60,25 +52,33 @@ public final class ProfileMenu {
 
 	@FXML
 	void onP1Pressed(final MouseEvent event) {
-		Main.switchRoot(Menu.getProfileSelect());
+		ProfileSelectMenu selectController = Menu.getProfileSelectController();
 		setProfileSelected(0);
+		selectController.setProfile();
+		Main.switchRoot(Menu.getProfileSelect());
 	}
 
 	@FXML
 	void onP2Pressed(final MouseEvent event) {
-		Main.switchRoot(Menu.getProfileSelect());
+		ProfileSelectMenu selectController = Menu.getProfileSelectController();
 		setProfileSelected(1);
+		selectController.setProfile();
+		Main.switchRoot(Menu.getProfileSelect());
 	}
 
 	@FXML
 	void onP3Pressed(final MouseEvent event) {
-		Main.switchRoot(Menu.getProfileSelect());
+		ProfileSelectMenu selectController = Menu.getProfileSelectController();
 		setProfileSelected(2);
+		selectController.setProfile();
+		Main.switchRoot(Menu.getProfileSelect());
 	}
 
 	@FXML
 	void onP4Pressed(final MouseEvent event) {
-		Main.switchRoot(Menu.getProfileSelect());
+		ProfileSelectMenu selectController = Menu.getProfileSelectController();
 		setProfileSelected(3);
+		selectController.setProfile();
+		Main.switchRoot(Menu.getProfileSelect());
 	}
 }
