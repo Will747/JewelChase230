ffb5e38c86f29b405fdb296bfc6c83083295dbbe
Will747 <me@wkaye.dev>
2022-12-08 20:02:03 +0000

    Update HighScoreTableTests.java

--- src/test/java/JewelChaseTests/HighScoreTableTests.java
+++ src/test/java/JewelChaseTests/HighScoreTableTests.java
@@ -4,11 +4,10 @@ import com.example.jewelchase230.profiles.HighScoreTable;
 import com.example.jewelchase230.profiles.Profile;
 import org.junit.jupiter.api.Test;
 
-
 public class HighScoreTableTests {
     @Test
     public void readAndSaveTest() {
-        Profile testProfile = new Profile(1, 1, "Test", -1);
+        Profile testProfile = new Profile("Test");
 
         HighScoreTable highScoreTable = HighScoreTable.read();
 
