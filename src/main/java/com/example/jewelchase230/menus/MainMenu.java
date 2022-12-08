b55fb17ca653020905919385d623f2c289d2cde5
Adam <adam_smith_03@outlook.com>
2022-12-08 12:44:56 +0000

    Fixes

--- src/main/java/com/example/jewelchase230/menus/MainMenu.java
+++ src/main/java/com/example/jewelchase230/menus/MainMenu.java
@@ -29,13 +29,13 @@ public final class MainMenu {
 
     @FXML
     private void onSettingsMenuPressed(final MouseEvent event) {
-        Menu.getSettingsMenuController().setPreviousParent(Menu.getPauseMenu());
+        Menu.getSettingsMenuController().setPreviousParent(Menu.getMainMenu());
         Main.switchRoot(Menu.getSettingsMenu());
     }
 
     @FXML
     private void onHighScoreTablePressed(final MouseEvent event) {
-        Menu.getHighScoreMenuController().setPreviousParent(Menu.getPauseMenu());
+        Menu.getHighScoreMenuController().setPreviousParent(Menu.getMainMenu());
         Main.switchRoot(Menu.getHighScoreTable());
     }
 
