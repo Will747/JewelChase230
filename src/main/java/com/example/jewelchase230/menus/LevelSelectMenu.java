e86b8b1e762a35345d415a395c9b50a7afc14eff
Dowen212 <2124746@swansea.ac.uk>
2022-12-09 00:27:56 +0000

    Level Select functioning

--- src/main/java/com/example/jewelchase230/menus/LevelSelectMenu.java
+++ src/main/java/com/example/jewelchase230/menus/LevelSelectMenu.java
@@ -6,16 +6,39 @@ import com.example.jewelchase230.Main;
 import com.example.jewelchase230.Menu;
 import com.example.jewelchase230.profiles.Profile;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
 /**
  * Test menu to test out switching between menus.
  *
- * @author Will Kaye
+ * @author Daniel Clark, Will Kaye
  */
+
 public final class LevelSelectMenu {
     /** Profile being used to play the levels. */
     private Profile selectedProfile;
+
+    @FXML
+    private Button lvl1;
+    @FXML
+    private Button lvl2;
+    @FXML
+    private Button lvl3;
+    @FXML
+    private Button lvl4;
+    @FXML
+    private Button lvl5;
+    @FXML
+    private Button lvl6;
+    @FXML
+    private Button lvl7;
+    @FXML
+    private Button lvl8;
+    @FXML
+    private Button lvl9;
+    @FXML
+    private Button lvl10;
 
     @FXML
     void onBackPressed(final MouseEvent event) {
@@ -24,14 +47,16 @@ public final class LevelSelectMenu {
 
     @FXML
     void changeLevel(final MouseEvent event) {
-        Main.switchRoot(Menu.getLevelSelectMenu());
+        int btnNum = Integer.parseInt(((Button)event.getSource()).getText());
+        System.out.println(btnNum);
+        switchToLevel(btnNum);
     }
 
-    //temporary tester
+    /*temporary tester
     @FXML
     void levelOne(final MouseEvent event) {
         switchToLevel(1);
-    }
+    }*/
 
     /**
      * @param profile The profile being used.
@@ -41,9 +66,10 @@ public final class LevelSelectMenu {
     }
 
     private void switchToLevel(final int levelNum) {
-        Level nextLevel = LevelFileReader.getLevel(1);
+        Level nextLevel = LevelFileReader.getLevel(levelNum);
         nextLevel.setProfile(selectedProfile);
         Main.setLevel(nextLevel);
         Main.switchToCanvas();
     }
+    
 }
